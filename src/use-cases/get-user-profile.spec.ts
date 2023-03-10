64b2578b04ab5644965a1af25ed0071318dce7a0
andreenakashima <andreenakashima@gmail.com>
2023-03-10 14:45:13 -0300

    Add user profile use case

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,37 @@
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { hash } from "bcryptjs";
+import { beforeEach, describe, expect, it } from "vitest";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { GetUserProfile } from "./get-user-profile";
+
+let usersRepository: InMemoryUsersRepository;
+let sut: GetUserProfile;
+
+describe("Get User Profile Use Case", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new GetUserProfile(usersRepository);
+  });
+
+  it("should to be able to get user profile", async () => {
+    const createdUser = await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password_hash: await hash("123123", 6),
+    });
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    });
+
+    expect(user.name).toEqual("John Doe");
+  });
+
+  it("should not be able to get user profile with wrong id", async () => {
+    expect(() =>
+      sut.execute({
+        userId: "non-exist-id",
+      })
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+});
