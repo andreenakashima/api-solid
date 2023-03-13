a15b489f4e06f24f26a5c18726edc11531fcd47e
andreenakashima <andreenakashima@gmail.com>
2023-03-13 10:11:52 -0300

    Add config JWT

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
 import { ZodError } from "zod";
 import { env } from "./env";
@@ -5,20 +6,24 @@ import { appRoutes } from "./http/routes";
 
 export const app = fastify();
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+});
+
 app.register(appRoutes);
 
 app.setErrorHandler((error, _, reply) => {
-	if (error instanceof ZodError) {
-		return reply
-			.status(400)
-			.send({ message: "Validation error.", issues: error.format() });
-	}
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error.", issues: error.format() });
+  }
 
-	if (env.NODE_ENV !== "production") {
-		console.error(error);
-	} else {
-		// Here we should log to an external tool like Datadog/NewRelic/Sentry
-	}
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // Here we should log to an external tool like Datadog/NewRelic/Sentry
+  }
 
-	return reply.status(500).send({ message: "Internal server error." });
+  return reply.status(500).send({ message: "Internal server error." });
 });
